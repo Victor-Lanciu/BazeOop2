db2257f4a6406b6f01d9a64fb2f14064c9da1cfb
Victor Lanciu <wlanciu@gmail.com>
2024-12-20 11:39:21 +0200

    Added first problem.

---
+++ Problema1/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    public class Employee
+    {
+        public string Name;
+        public int ID;
+        public decimal Salary;
+
+        public Employee(string name, int id, decimal salary)
+        {
+            Name = name;
+            ID = id;
+            Salary = salary;
+        }
+        public virtual decimal CalculateAnnualSalary()
+        {
+            return 12 * Salary;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name:{Name}, ID: {ID}, Annual Salary:{CalculateAnnualSalary()}");
+        }
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public decimal Bonus { get; set; }
+        public FullTimeEmployee(string name, int id, decimal salary, decimal bonus) : base(name, id, salary)
+        {
+            Bonus = bonus;
+        }
+        public override decimal CalculateAnnualSalary()
+        {
+            return Salary * 12 + Bonus;
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+        public PartTimeEmployee(string name,int id,decimal hourlyRate):base(name,id, hourlyRate)
+        {
+            HourlyRate = hourlyRate;
+        }
+        public override decimal CalculateAnnualSalary()
+        {
+            return (HourlyRate * 4) * 5 * 52; //4 ore de lucru pe zi, 5 zile pe saptamana, 52 saptamani
+        }
+
+    }
+}
---
+++ Problema1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Problema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee employeeOne = new FullTimeEmployee("Igor", 101, 1500, 1500);
+            employeeOne.DisplayInfo();
+            PartTimeEmployee employeeTwo = new PartTimeEmployee("Gabriel", 102, 12);
+            employeeTwo.DisplayInfo();
+        }
+    }
+}
